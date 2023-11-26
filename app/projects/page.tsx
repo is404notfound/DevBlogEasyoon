17da405d34d6d25a57c25c291fed8fee2bd8c3e4
easyoon <easyoon@wemakeprice.com>
2023-11-26 19:02:46 +0900

    [#8] Set Images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            These are my github repository projects :D It's still small, but I'll fill it up with more!
           </p>
         </div>
         <div className="container py-12">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,14 +1,14 @@
 const projectsData = [
   {
-    title: 'React Dev Blog!',
+    title: 'My Tech Blog!',
     description: `This is my personal blog where I write about my experiences as a developer. I write about my experiences with React, Next.js, and other web development topics.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: './static/images/blog-project-image.png',
     href: 'https://github.com/is404notfound/DevBlogEasyoon',
   },
   {
-    title: 'Study for Data Structure!',
+    title: 'Data Structure Team Study!',
     description: `This is my study source for Data Structure. I study about Data Structure with my team. We study about Data Structure with JS. And I describe about this source in Blog tab!`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: './static/images/study-project-image.png',
     href: 'https://github.com/is404notfound/03_TEAM_STUDY_DATA_STRUCTURE',
   },
 ]
