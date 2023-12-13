c2c40f5d07e9ead4a907531ed2e9ad1be7977e95
easyoon <easyoon@wemakeprice.com>
2023-12-13 15:22:20 +0900

    [#6] 'next/router' => 'next/navigation' 변경 (next 13v변경 사항)

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -1,16 +1,19 @@
 'use client'
 
 import { useTranslation } from 'react-i18next';
+import { usePathname } from 'next/navigation';
 
 const LanguageSelector = ({ router }) => {
   const { t, i18n } = useTranslation();
+  const currentPath = usePathname();
 
   const changeLanguage = (e) => {
     const selectedLanguage = e.target.value;
 
     i18n.changeLanguage(selectedLanguage);
+    localStorage.setItem('selectedLanguage', selectedLanguage);
     if (router) {
-        router.push('/', '/', { locale: selectedLanguage });
+        router.push(currentPath, currentPath, { locale: selectedLanguage });
       }
   };
 
