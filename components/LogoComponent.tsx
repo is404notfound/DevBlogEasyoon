e7df084b9e4df0c4db4041ba04e924b38cfd7e54
easyoon <easyoon@wemakeprice.com>
2023-12-14 16:25:42 +0900

    [#16] Add CSS Effects :: Dancing Cat Logo Effect

---
+++ components/LogoComponent.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components'
+
+const waveAnimation = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+`;
+
+// 로고 컨테이너에 대한 스타일
+const LogoContainer = styled.div`
+  position: relative;
+  width: 100px;
+  height: 100px;
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.1);
+    opacity: 0.8;
+  }
+`;
+
+// 로고 이미지에 대한 스타일
+const LogoImage = styled.img`
+  width: auto;
+  height: auto;
+`;
+
+const Wave = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: url(https://i.pinimg.com/originals/9a/45/d9/9a45d9cfc41af280fa36ae0d4b834e65.gif) repeat-x bottom;
+  background-size: 100% 100%;  /* 배경 이미지 크기 조절 */
+  animation: ${waveAnimation} 2s linear infinite;
+`;
+
+// 오버레이 텍스트에 대한 스타일
+const OverlayText = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  padding: 10px;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+const LogoComponent = ({ src }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <LogoContainer
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+    >
+      <LogoImage
+        src={src}
+        alt="Logo"
+      />
+      {isHovered && <Wave />}
+      {isHovered || (
+        <OverlayText>
+          Hover over me!
+        </OverlayText>
+      )}
+    </LogoContainer>
+  );
+};
+
+export default LogoComponent;
