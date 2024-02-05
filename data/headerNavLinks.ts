db462b4e16528753fdac2833fa67b0a18e6785ce
easyoon <easyoon@wemakeprice.com>
2024-02-05 18:55:13 +0900

    Add new style sheet and update tabs order

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/archive', title: 'Archive' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/archive', title: 'Archive (*)' },
+  { href: '/dashboard', title: 'Dashboard (*)'},
 ]
 
 export default headerNavLinks
