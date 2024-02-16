a3cd57423cbbec2783e7ff8a6098e9feb2e5fa4d
easyoon <easyoon@wemakeprice.com>
2024-02-16 23:39:45 +0900

    [#22] Add Code Records Graph in Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import Card from '@/components/dashboard/Card';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useTotalCodeRecords from 'hooks/useTotalCodeRecords'
 import { allBlogs } from 'contentlayer/generated'
+import LineGraph from '@/components/LineGraph';
 
 const Dashboard = () => {
   const { totalCodeRecords, latestRecord } = useTotalCodeRecords();
+  const [codeCounts, setCodeCounts] = useState<number[]>([]);
+  const [dates, setDates] = useState<string[]>([]);
+
+  useEffect(() => {
+    setCodeCounts(totalCodeRecords.map((record) => record.codeCount));
+    setDates(totalCodeRecords.map((record) => record.date.slice(5)));
+  }, [totalCodeRecords]);
+
     
   return (
     <div className="flex flex-col h-screen">
@@ -20,6 +29,16 @@ const Dashboard = () => {
           <Card title="세상으로 나간 코드" description="[줄]" content={latestRecord.codeCount || '??'} />
           <Card title="쓴 글" description="[편]" content={allBlogs.length} />
           <Card title="인상 깊었던 글" description="[편]" content="집계 중" fontSizeLevel={'3xl'} />
+        </div>
+        <div className="grid grid-cols-1 mt-6 mb-6 pt-6">
+          <LineGraph
+            xAxisData={dates}
+            yAxisData={codeCounts}
+            yAxisLabel='Code Count'
+            title={`${dates.length + 1}일간의 코드량 변화`}
+            description={`2024-${dates[0]}부터 2024-${dates[dates.length - 1]}까지의 코드 작성량 변화 추이`}
+            point={`${codeCounts[codeCounts.length - 1] - codeCounts[0]}줄`}
+            />
         </div>
         <div className="grid grid-cols-1 gap-6 pt-6 ">
           <Card title="SEO 실적" description="[키워드 / 순위]" content="- / -" />
