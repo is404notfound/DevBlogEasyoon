89d0d3312e5fbca7568cf2b772892645a16464ee
easyoon <easyoon@wemakeprice.com>
2024-02-16 18:32:37 +0900

    [#22] Update Script

--- code-records-data-generator.sh
+++ code-records-data-generator.sh
@@ -7,19 +7,23 @@
 # 현재 날짜를 YYYY-MM-DD 형식으로 저장
 date=$(date +'%Y-%m-%d')
 
-# 이전 기록이 있는지 확인
-previous_record=$(grep "^$date|" code-records-data.json)
+saved_contents=$(cat code-records-data.json)
+
+# 이전에 같은 date 기록이 있는지 확인
+duplicated_record=$(echo $saved_contents | grep -o \"$date\":[0-9]*)
 
 # 라인 수를 계산하고 결과를 저장 (grep : 파일 내 특정 문자열을 찾음)
 lines=$(git ls-files | grep -E '\.(tsx|ts|js|css|json|sh)$' | xargs wc -l | tail -n 1 | awk '{print $1}')
 
-# 이전 기록이 있는 경우에는 해당 기록을 업데이트 (-n : 문자열의 길이가 0인지 반환)
-if [ -n "$previous_record" ]; then
-  # 새로운 파일을 생성하여 기존 파일의 내용을 변경
-  grep -v "^$date:" code-records-data.json > temp.json
-  echo {\"$date\":$lines} >> temp.json
-  mv temp.json code-records-data.json
-else
-  # 이전 기록이 없는 경우에는 새로운 줄로 추가
+if [ ! -s code-records-data.json ]; then
   echo {\"$date\":$lines} >> code-records-data.json
+
+elif [ -n "$duplicated_record" ]; then
+  new_contents=$(echo $saved_contents | sed "s/$duplicated_record/\"$date\":$lines/")
+  echo $new_contents > code-records-data.json
+
+else
+  new_contents=$(echo $saved_contents | sed "s/}/, \"$date\":$lines}/")
+  echo $new_contents > code-records-data.json
 fi
+
