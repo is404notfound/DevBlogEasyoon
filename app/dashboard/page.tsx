bef88e3e76a70b68714a92e80aeab91ab8df0d55
easyoon <easyoon@wemakeprice.com>
2024-02-16 20:02:45 +0900

    [#22] Add to get count of posts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import Card from '@/components/dashboard/Card';
 import React, { useEffect } from 'react';
 import useTotalCodeRecords from 'hooks/useTotalCodeRecords'
+import { allBlogs } from 'contentlayer/generated'
 
 const Dashboard = () => {
   const { totalCodeRecords, latestRecord } = useTotalCodeRecords();
@@ -17,7 +18,7 @@ const Dashboard = () => {
       <main className="flex-1 p-6">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <Card title="세상으로 나간 코드" description="[줄]" content={latestRecord.codeCount || '??'} />
-          <Card title="쓴 글" description="[편]" content="36" />
+          <Card title="쓴 글" description="[편]" content={allBlogs.length} />
           <Card title="인상 깊었던 글" description="[편]" content="집계 중" fontSizeLevel={'3xl'} />
         </div>
         <div className="grid grid-cols-1 gap-6 pt-6 ">
--- components/dashboard/Card.tsx
+++ components/dashboard/Card.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-const Card = ({ title, description, content, fontSizeLevel = '5xl' }: {
+const Card = ({ title, description, content, fontSizeLevel = '6xl' }: {
     title?: string;
     description?: string;
     content?: string | number ;
@@ -9,9 +9,10 @@ const Card = ({ title, description, content, fontSizeLevel = '5xl' }: {
     return (
         <div className="bg-white bg-opacity-70 rounded-lg shadow-md p-6 border-dotted border-4 border-pink-500 shadow-lg shadow-pink-500/50">
             <h1 className="text-lg font-semibold mb-4">{ title }</h1>
-            <p className="text-sm font-semibold mb-4 text-right">{ description }</p>
-            <p className={`text-${fontSizeLevel} text-gray-600 text-center`}>{ content } </p>
+            <p className="text-sm font-semibold mb-4 text-right">{ description } </p>
+            <p className={`text-${ fontSizeLevel } text-gray-600 text-center`}>{ content } </p>
         </div>
-    )}
+    )
+}
 
 export default Card
