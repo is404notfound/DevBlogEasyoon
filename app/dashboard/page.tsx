5b9c7ee9a06fc1fe5f1a7ddb29d36adae21a1d8d
easyoon <easyoon@wemakeprice.com>
2024-02-20 15:08:01 +0900

    Adjust dashboard page width, add language change

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -22,32 +22,38 @@ const Dashboard = () => {
   }, [codeCounts]);
     
   return (
-    <div className="flex flex-col h-screen">
-      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Dashboard \\\\\\\\\\\\\\\\
           </h1> 
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            Hello, World!
+          </p>
         </div>
-      <main className="flex-1 p-6">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <Card title="세상으로 나간 코드" description="[줄]" content={latestRecord.codeCount || '??'} />
-          <Card title="쓴 글" description="[편]" content={allBlogs.length} />
-          <Card title="인상 깊었던 글" description="[편]" content="집계 중" fontSizeLevel={'3xl'} />
-        </div>
-        <div className="grid grid-cols-1 mt-6 mb-6 pt-6">
-          <LineGraph
-              xAxisData={dates}
-              yAxisData={codeCounts}
-              yAxisLabel='Code Count'
-              title={`${dates.length}일간의 코드량 변화`}
-              description={`2024-${dates[0]}부터 2024-${dates[dates.length - 1]}까지의 코드 작성량 변화 추이`}
-              point={diff > 0 ? `+${diff}줄` : `${diff}줄`}
-            />
-        </div>
-        <div className="grid grid-cols-1 gap-6 pt-6 ">
-          <Card title="SEO 실적" description="[키워드 / 순위]" content="- / -" />
-        </div>
-      </main>
+        <div className="container py-12">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <Card title="세상으로 나간 코드" description="[줄]" content={latestRecord.codeCount || '??'} />
+            <Card title="쓴 글" description="[편]" content={allBlogs.length} />
+            <Card title="인상 깊었던 글" description="[편]" content="집계 중" fontSizeLevel={'3xl'} />
+          </div>
+          <div className="grid grid-cols-1 mt-6 mb-6 pt-6">
+            <LineGraph
+                xAxisData={dates}
+                yAxisData={codeCounts}
+                yAxisLabel='Code Count'
+                title={`${dates.length}일간의 코드량 변화`}
+                description={`2024-${dates[0]}부터 2024-${dates[dates.length - 1]}까지의 코드 작성량 변화 추이`}
+                point={diff > 0 ? `+${diff}줄` : `${diff}줄`}
+              />
+          </div>
+          <div className="grid grid-cols-1 gap-6 pt-6 ">
+            <Card title="History" description="작업 내역 (Commit)" content="-" />
+          </div>
+          <div className="grid grid-cols-1 gap-6 pt-6 ">
+            <Card title="SEO 실적" description="[키워드 / 순위]" content="- / -" />
+          </div>
+      </div>
     </div>
   );
 }
