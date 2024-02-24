106ad62d0b28e6ba6cd2c8393c72ead24a8e1fa6
easyoon <easyoon@wemakeprice.com>
2024-02-24 22:18:56 +0900

    [#29] Code Line 수 를 계산할 때, *.json파일 제외

--- generators/code-records-data-generator.sh
+++ generators/code-records-data-generator.sh
@@ -1,6 +1,6 @@
 # 빌드 시 동작하는 스크립트 입니다.
 # 프로젝트 내 아래 확장자의 파일들의 총 라인 수를 구합니다.
-# 포함 확장자 : tsx|ts|js|css|json|sh
+# 포함 확장자 : tsx|ts|js|css|sh
 # 나온 값을 컴포넌트에서 파싱할 수 있도록 빌드 한 날짜와 함께 txt 파일로 저장합니다.
 
 echo "LOG :: Generating code records data..."
@@ -16,7 +16,7 @@ saved_contents=$(cat "$output_file")
 duplicated_record=$(echo $saved_contents | grep -o \"$date\":[0-9]*)
 
 # 라인 수를 계산하고 결과를 저장 (grep : 파일 내 특정 문자열을 찾음)
-lines=$(git ls-files | grep -E '\.(tsx|ts|js|css|json|sh)$' | xargs wc -l | tail -n 1 | awk '{print $1}')
+lines=$(git ls-files | grep -E '\.(tsx|ts|js|css|sh)$' | xargs wc -l | tail -n 1 | awk '{print $1}')
 
 if [ ! -s "$output_file" ]; then
   echo {\"$date\":$lines} >> "$output_file"
