c55f0cfdaf1601f4e8596ac4450de1d5e07ec051
easyoon <easyoon@wemakeprice.com>
2024-02-24 22:20:58 +0900

    Show counting file types on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -33,7 +33,7 @@ const Dashboard = () => {
         </div>
         <div className="container py-12">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <Card title="세상으로 나간 코드" description="[줄]" content={latestRecord.codeCount || '??'} />
+            <Card title="세상으로 나간 코드" description="[줄/(tsx,ts,js,css,sh)]" content={latestRecord.codeCount || '??'} />
             <Card title="쓴 글" description="[편]" content={allBlogs.length} />
             <Card title="인상 깊었던 글" description="[편]" content="집계 중" fontSizeLevel={'3xl'} />
           </div>
