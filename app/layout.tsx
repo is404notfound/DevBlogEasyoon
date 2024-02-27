255df5a6b4ff178f2520842ba66b2e16576d880b
easyoon <easyoon@wemakeprice.com>
2024-02-27 23:37:47 +0900

    [#16] Add Google Search Console meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -254,6 +254,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+        <meta name="google-site-verification" content="5l483OR37tc7--oH3yCgu74KMqTq7xeyirlmFxy0s4I" />
       </Head>
       <body className="bg-white text-black antialiased dark:bg-gray-800 dark:text-pink-400" style={{ textShadow: '0 0 30px rgba(255, 0, 255, 0.5)' }}>
       
