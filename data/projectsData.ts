07b3f024e8b39594b8df609b240834839fe5e8cf
easyoon <easyoon@wemakeprice.com>
2024-05-17 00:59:45 +0900

    [Update] Project data :: aws-certified-developer

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,25 +1,31 @@
 const projectsData = [
   {
+    title: '[On-Going] Aws Certified Developer-Associate Study!',
+    description: 'Preparing for exams to qualify for AWS Certified Developer-Associate.',
+    imgSrc: './static/images/aws-certificate-project.png',
+    href: 'https://github.com/is404notfound/aws-certified-developer-associate/blob/main/0_README.md',
+  },
+  {
     title: '[On-Going] 666-find-anomalies!',
-    description: `This is 3d game project with Three.js, React and RN.`,
+    description: 'This is 3d game project with Three.js, React and RN.',
     imgSrc: './static/images/666-find-anomalies-demo.png',
     href: 'https://github.com/is404notfound/666-find-anomalies',
   },
   {
     title: '[On-Going] Cook The Fridge!',
-    description: `This is my modern design recipe web application. I made this web application with my team.`,
+    description: 'This is my modern design recipe web application. I made this web application with my team.',
     imgSrc: './static/images/cook-the-fridge-demo.png',
     href: 'https://github.com/is404notfound/CookTheFridge',
   },
   {
     title: 'Dev Blog Easyoon!',
-    description: `This is my personal blog where I write about my experiences as a developer. I write about my experiences with React, Next.js, and other web development topics.`,
+    description: 'This is my personal blog where I write about my experiences as a developer. I write about my experiences with React, Next.js, and other web development topics.',
     imgSrc: './static/images/read-me-image.png',
     href: 'https://github.com/is404notfound/DevBlogEasyoon',
   },
   {
     title: 'Data Structure Team Study!',
-    description: `This is my study source for Data Structure. I study about Data Structure with my team. We study about Data Structure with JS. And I describe about this source in Blog tab!`,
+    description: 'This is my study source for Data Structure. I study about Data Structure with my team. We study about Data Structure with JS. And I describe about this source in Blog tab!',
     imgSrc: './static/images/study-project-image.png',
     href: 'https://github.com/is404notfound/03_TEAM_STUDY_DATA_STRUCTURE',
   },
