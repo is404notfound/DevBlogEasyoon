c6e25c390c7e7505472f30168f061bbc54c5f5ee
easyoon <this.console.log@gmail.com>
2024-12-12 19:11:00 +0900

    [Update] Header Translation etc.

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,10 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/calendar', title: 'Conferences' },
-  { href: '/archive', title: 'Archives' },
-  { href: '/blog', title: 'Blogs' },
-  { href: '/dashboard', title: 'Dashboard'},
-  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Posts' },
+  { href: '/archive', title: 'Bookmarks' },
+  { href: '/calendar', title: 'Dev-Calendar' },
+  { href: '/dashboard', title: 'Analytics' },
+  { href: '/projects', title: 'Github' },
   // { href: '/about', title: 'About' },
   // { href: '/tags', title: ''}
 ]
